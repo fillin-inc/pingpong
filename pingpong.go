851582f130b4e1043917998d3a465a5ff8ca8f47
Yuya Matsushima <yuyam@fillin-inc.com>
2017-07-21 18:29:02 +0900

    Add HandlerJSON

--- pingpong.go
+++ pingpong.go
@@ -1,16 +1,36 @@
 package pingpong
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 const PONG = "Pong"
+
+type jsonRes struct {
+	Msg       string    `json:"msg"`
+	CreatedAt time.Time `json:"created_at"`
+}
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 	w.Header().Set("Content-Length", strconv.Itoa(len(PONG)))
 	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, PONG)
+}
+
+func HandlerJSON(w http.ResponseWriter, r *http.Request) {
+	res, _ := json.Marshal(jsonRes{
+		Msg:       PONG,
+		CreatedAt: time.Now(),
+	})
+	resStr := string(res)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Length", strconv.Itoa(len(resStr)))
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, resStr)
 }
--- pingpong_test.go
+++ pingpong_test.go
@@ -1,6 +1,7 @@
 package pingpong
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -50,6 +51,49 @@ func TestHandler(t *testing.T) {
 			"Handler return unexpected Body(expected:%s actual:%s)",
 			e.Body,
 			string(body),
+		)
+	}
+}
+
+func TestHandlerJSON(t *testing.T) {
+	e := expected{
+		HTTPStatus:  http.StatusOK,
+		ContentType: "application/json",
+		Body:        PONG,
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(HandlerJSON))
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.StatusCode != e.HTTPStatus {
+		t.Errorf(
+			"Handler return unexpected HTTP Status(expected:%d actual:%d)",
+			e.HTTPStatus,
+			res.StatusCode,
+		)
+	}
+
+	if res.Header.Get("Content-Type") != e.ContentType {
+		t.Errorf(
+			"Handler return unexpected Content-Type(expected:%s actual:%s)",
+			e.ContentType,
+			res.Header.Get("Content-Type"),
+		)
+	}
+
+	body, _ := ioutil.ReadAll(res.Body)
+	j := jsonRes{}
+	json.Unmarshal(body, &j)
+	if j.Msg != e.Body {
+		t.Errorf(
+			"Handler return unexpected Body(expected:%s actual:%s)",
+			e.Body,
+			j.Msg,
 		)
 	}
 }
